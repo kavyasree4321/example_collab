1e023e0249406dab67ea7905329937298db2aff5
raamakeerthana <ramakeerthana128@gmail.com>
2025-10-15 16:43:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React app
         </a>
             <h2>HI Welcome to this website</h2>
       </header>
